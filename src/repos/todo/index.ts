471c2ca0ccdcb2e705ae8707fe5dd5dfce9e771a
sakibstark11 <16sakib@gmail.com>
2024-02-13 12:06:10 +0000

    wip

--- src/repos/todo/index.ts
+++ src/repos/todo/index.ts
@@ -1,10 +1,10 @@
 import httpErrorMap from "../../utils/httpErrorMap";
 
-export default function todo(abortController?: any) {
+export default function todo(abortSignal?: AbortSignal) {
     const HOST_URL = "https://jsonplaceholder.typicode.com/todos";
     const getTodoById = async (id: string): Promise<Todo> => {
         const request = await fetch(`${HOST_URL}/${id}`, {
-            signal: abortController.signal,
+            signal: abortSignal,
         });
         if (request.status !== 200) {
             throw Error(httpErrorMap[request.status]);
