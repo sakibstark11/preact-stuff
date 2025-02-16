3440d51678e6849a75904c3eaaa8147ec2228123
Sakib Alam <47223230+sakibstark11@users.noreply.github.com>
2025-02-16 18:56:35 +0000

    Update index.tsx

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,37 +1,35 @@
 import { useEffect } from "preact/hooks";
 import Loader from "../../components/Loader/index";
 import Search from "../../components/Search";
-import useAbortController from "../../hooks/useAbortController";
 import todoRepo from "../../repos/todo";
 import useFetchData from "../../hooks/useFetchData";
 import TodoDisplay from "../../components/TodoDisplay";
 
 export default function Home() {
-    const getAbortSignal = useAbortController();
-    const { getTodoById } = todoRepo(getAbortSignal);
+  const { getTodoById } = todoRepo();
+  const dataFetch = (id: string, signal: AbortSignal) =>
+    getTodoById(id, signal);
 
-    const [executeFunction, loading, data, error] = useFetchData((id) =>
-        getTodoById(id)
-    );
+  const [executeFunction, loading, data, error] = useFetchData(dataFetch);
 
-    const handleSearch = (id: string) => {
-        executeFunction(id);
-    };
+  const handleSearch = (id: string) => {
+    executeFunction(id);
+  };
 
-    useEffect(() => {
-        executeFunction("5");
-    }, [executeFunction]);
+  useEffect(() => {
+    executeFunction("5");
+  }, []);
 
-    return (
-        <>
-            <Search onChange={handleSearch} />
+  return (
+    <>
+      <Search onChange={handleSearch} />
 
-            {loading && <Loader />}
-            <br />
-            {error && <h4>{error}</h4>}
-            <br />
-            {!loading && data && <TodoDisplay todo={data} />}
-            <br />
-        </>
-    );
+      {loading && <Loader />}
+      <br />
+      {error && <h4>{error}</h4>}
+      <br />
+      {!loading && data && <TodoDisplay todo={data} />}
+      <br />
+    </>
+  );
 }
