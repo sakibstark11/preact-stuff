93e9b0669b7da008fbbcb5c9ba2f5418ff1f794b
Sakib Alam <47223230+sakibstark11@users.noreply.github.com>
2025-02-16 18:56:55 +0000

    Update useFetchData.ts

--- src/hooks/useFetchData.ts
+++ src/hooks/useFetchData.ts
@@ -1,4 +1,4 @@
-import { useCallback, useState, useRef } from "react";
+import { useState, useRef, useCallback } from "react";
 
 export default function useFetchData<DataFetchType>(
     dataFetchFunction: (...args: any[]) => Promise<DataFetchType>
@@ -6,24 +6,27 @@ export default function useFetchData<DataFetchType>(
     const [loading, setLoading] = useState(false);
     const [data, setData] = useState<DataFetchType>(null);
     const [error, setError] = useState<string>(null);
-    const dataFetchFunctionRef = useRef(dataFetchFunction);
+    const abortControllerRef = useRef<AbortController>(null);
 
-    const executeFunction = useCallback(
-        async (...args: any[]) => {
-            setLoading(true);
-            setError(null);
-            try {
-                const result = await dataFetchFunctionRef.current(...args);
-                setData(result);
-            } catch (err) {
-                setData(null);
-                setError((err as Error).message || "An error occurred");
-            } finally {
-                setLoading(false);
-            }
-        },
-        [dataFetchFunctionRef]
-    );
+    const executeFunction = useCallback(async (...args: any[]) => {
+        if (abortControllerRef.current) {
+            abortControllerRef.current.abort();
+        }
+        abortControllerRef.current = new AbortController();
+
+        setLoading(true);
+        setError(null);
+
+        try {
+            const result = await dataFetchFunction(...args, abortControllerRef.current.signal);
+            setData(result);
+        } catch (err) {
+            setData(null);
+            setError((err as Error).message || "An error occurred");
+        } finally {
+            setLoading(false);
+        }
+    }, [dataFetchFunction])
 
     return [executeFunction, loading, data, error] as const;
 }
